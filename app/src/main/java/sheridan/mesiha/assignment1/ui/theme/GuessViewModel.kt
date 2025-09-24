39ddf06001df250b6388833f9ce35b9199aa4672
Elodie Mesiha <mesiha@sheridancollege.ca>
2025-09-24 18:10:52 -0400

    Updated GuessViewModel.kt to use stateFlow

--- app/src/main/java/sheridan/mesiha/assignment1/ui/theme/GuessViewModel.kt
+++ app/src/main/java/sheridan/mesiha/assignment1/ui/theme/GuessViewModel.kt
@@ -1,22 +1,35 @@
 package sheridan.mesiha.assignment1.ui.theme
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import kotlin.random.Random
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class GuessViewModel {
-    private var secretNumber by mutableStateOf(Random.nextInt(1,11))
+data class UiState(
+    val message: String = "Guess a number between 1 and 10",
+    val gameOver: Boolean = false
+)
+class GuessViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
 
-    var guess by mutableStateOf("")
-    var message by mutableStateOf("Guess a number between 1 and 10")
-    var gameOver by mutableStateOf(false)
+    private var secretNumber = Random.nextInt(1,11)
 
-    fun submitGuess(){
+    fun makeGuess(guess: Int){
+        when{
+            guess < secretNumber -> _uiState.value = _uiState.value.copy(message = "The number is larger")
+            guess > secretNumber -> _uiState.value = _uiState.value.copy(message = "The number is smaller")
+            else -> _uiState.value = _uiState.value.copy(message = "Correct. The number was $secretNumber", gameOver = true)
 
+        }
     }
 
     fun resetGame(){
-
+        secretNumber = Random.nextInt(1,11)
+        _uiState.value = UiState()
     }
 }
