94f4488bdbf4518bc8e311b5a53937b8d6d21070
Elodie Mesiha <mesiha@sheridancollege.ca>
2025-09-24 22:20:29 -0400

    defined custom colors

--- app/src/main/java/sheridan/mesiha/assignment1/ui/theme/Color.kt
+++ app/src/main/java/sheridan/mesiha/assignment1/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package sheridan.mesiha.assignment1.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightPink = Color(0xFFFFC1E3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val HotPink = Color(0xFFFF1493)
+
+val GreyBackground = Color(0xFFB0B0B0)
+
+val WhiteSurface = Color(0xFFFFFFFF)
